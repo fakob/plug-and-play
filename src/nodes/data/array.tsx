31f1fa4f7404f0ecbcad03434a843b6262dc9116
Tobias Elinder <tobiaselinder@gmail.com>
2021-08-17 20:51:47 +0200

    added array get

--- src/nodes/data/array.tsx
+++ src/nodes/data/array.tsx
@@ -2,14 +2,15 @@ import PPNode from '../../classes/NodeClass';
 import Socket from '../../classes/SocketClass';
 import { DATATYPE, SOCKET_TYPE } from '../../utils/constants';
 
-const inElementName = 'Element';
-const outArrayName = 'Array';
+const elementName = 'Element';
+const arrayName = 'Array';
+const indexName = 'Index';
 
-export class CreateArray extends PPNode {
+export class ArrayCreate extends PPNode {
   protected getDefaultIO(): Socket[] {
     return [
-      new Socket(SOCKET_TYPE.IN, inElementName, DATATYPE.ANY),
-      new Socket(SOCKET_TYPE.OUT, outArrayName, DATATYPE.ARRAY),
+      new Socket(SOCKET_TYPE.IN, elementName, DATATYPE.ANY),
+      new Socket(SOCKET_TYPE.OUT, arrayName, DATATYPE.ARRAY),
     ];
   }
   protected async onExecute(
@@ -17,10 +18,29 @@ export class CreateArray extends PPNode {
     outputObject: Record<string, unknown>
   ): Promise<void> {
     const elements = Object.keys(inputObject).map((key) => inputObject[key]);
-    outputObject[outArrayName] = elements;
+    outputObject[arrayName] = elements;
   }
 
   getCanAddInput(): boolean {
     return true;
+  }
+}
+
+export class ArrayGet extends PPNode {
+  protected getDefaultIO(): Socket[] {
+    return [
+      new Socket(SOCKET_TYPE.IN, arrayName, DATATYPE.ARRAY),
+      new Socket(SOCKET_TYPE.IN, indexName, DATATYPE.NUMBER, 0, true, {
+        round: true,
+        minValue: 1000,
+      }),
+      new Socket(SOCKET_TYPE.OUT, elementName, DATATYPE.ANY),
+    ];
+  }
+  protected async onExecute(
+    inputObject: unknown,
+    outputObject: Record<string, unknown>
+  ): Promise<void> {
+    outputObject[elementName] = inputObject[arrayName][inputObject[indexName]];
   }
 }
