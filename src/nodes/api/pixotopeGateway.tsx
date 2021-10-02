2c5d1174ed302b189744f416c5fb62871b6f79ad
Tobias Elinder <tobiaselinder@gmail.com>
2021-10-02 20:50:05 +0200

    specific saveimage node

--- src/nodes/api/pixotopeGateway.tsx
+++ src/nodes/api/pixotopeGateway.tsx
@@ -1,7 +1,9 @@
 import PPNode, { UpdateBehaviour } from '../../classes/NodeClass';
 import Socket from '../../classes/SocketClass';
 import { NODE_TYPE_COLOR, SOCKET_TYPE } from '../../utils/constants';
+import { ImageType } from '../datatypes/imageType';
 import { JSONType } from '../datatypes/jsonType';
+import { NumberType } from '../datatypes/numberType';
 import { StringType } from '../datatypes/stringType';
 
 const targetName = 'Target';
@@ -10,6 +12,10 @@ const methodName = 'Method';
 const paramsName = 'Params';
 const valueName = 'Value';
 const outputContentName = 'Content';
+const nodeName = 'Node';
+const scaleName = 'Scale';
+
+const gatewayAddress = 'http://localhost:16208/gateway/2.0.0/publish';
 
 export class PixotopeGatewayGet extends PPNode {
   // default to poll on interval X seconds
@@ -29,7 +35,7 @@ export class PixotopeGatewayGet extends PPNode {
   ): Promise<void> {
     const target = inputObject[targetName];
     const name = inputObject[nameName];
-    const res = await fetch('http://localhost:16208/gateway/2.0.0/publish', {
+    const res = await fetch(gatewayAddress, {
       method: 'POST',
       body: JSON.stringify({
         Topic: {
@@ -42,7 +48,7 @@ export class PixotopeGatewayGet extends PPNode {
       }),
     });
 
-    outputObject[outputContentName] = (await res.json()).Message.Value;
+    outputObject[outputContentName] = (await res.json())?.Message?.Value;
   }
 }
 
@@ -71,7 +77,7 @@ export class PixotopeGatewaySet extends PPNode {
     const name = inputObject[nameName];
     const value = inputObject[valueName];
 
-    fetch('http://localhost:16208/gateway/2.0.0/publish', {
+    fetch(gatewayAddress, {
       method: 'POST',
       body: JSON.stringify({
         Topic: { Type: 'Set', Target: target, Name: name },
@@ -82,16 +88,10 @@ export class PixotopeGatewaySet extends PPNode {
 }
 
 export class PixotopeGatewayCall extends PPNode {
-  // default to poll on interval X seconds
   protected getDefaultIO(): Socket[] {
     return [
       new Socket(SOCKET_TYPE.IN, targetName, new StringType(), 'Store'),
-      new Socket(
-        SOCKET_TYPE.IN,
-        methodName,
-        new StringType(),
-        'AddDefaultCamera'
-      ),
+      new Socket(SOCKET_TYPE.IN, methodName, new StringType(), 'VideoIO'),
       new Socket(SOCKET_TYPE.IN, paramsName, new JSONType(), {
         Fingerprint: 'Playground',
         Name: 'PlaygroundCamera',
@@ -107,13 +107,53 @@ export class PixotopeGatewayCall extends PPNode {
     const method = inputObject[methodName];
     const params = inputObject[paramsName];
 
-    const res = await fetch('http://localhost:16208/gateway/2.0.0/publish', {
+    const res = await fetch(gatewayAddress, {
       method: 'POST',
       body: JSON.stringify({
         Topic: { Type: 'Call', Target: target, Method: method },
         Message: { Params: params },
       }),
     });
-    outputObject[outputContentName] = (await res.json()).Message.Result;
+    outputObject[outputContentName] = (await res.json())?.Message?.Result;
+  }
+}
+
+export class PixotopeGatewayCallSaveImage extends PPNode {
+  protected getDefaultIO(): Socket[] {
+    return [
+      new Socket(SOCKET_TYPE.IN, targetName, new StringType(), 'Pipeline'),
+      new Socket(SOCKET_TYPE.IN, nodeName, new StringType(), 'XXX'),
+      new Socket(
+        SOCKET_TYPE.IN,
+        scaleName,
+        new NumberType(false, 0.1, 1.0, 0.01),
+        0.1
+      ),
+      new Socket(SOCKET_TYPE.OUT, outputContentName, new ImageType(), ''),
+    ];
+  }
+  protected async onExecute(
+    inputObject: any,
+    outputObject: Record<string, unknown>
+  ): Promise<void> {
+    const res = await fetch(gatewayAddress, {
+      method: 'POST',
+      body: JSON.stringify({
+        Topic: {
+          Type: 'Call',
+          Target: inputObject[targetName],
+          Method: 'SaveImage',
+        },
+        Message: {
+          Params: {
+            Nodes: [inputObject[nodeName]],
+            Scale: inputObject[scaleName],
+          },
+        },
+      }),
+    });
+    outputObject[outputContentName] = (
+      await res.json()
+    )?.Message?.Result?.Image;
   }
 }
