86404d8c57bdb3cd11417e914b1ef3a76ceed40a
Tobias Elinder <tobiaselinder@gmail.com>
2021-12-13 22:21:18 +0100

    better_get_and_post

--- src/nodes/api/get.tsx
+++ src/nodes/api/get.tsx
@@ -1,11 +1,13 @@
-import PPGraph from '../../classes/GraphClass';
 import PureNode, { UpdateBehaviour } from '../../classes/NodeClass';
 import Socket from '../../classes/SocketClass';
-import { NODE_TYPE_COLOR, SOCKET_TYPE } from '../../utils/constants';
+import { SOCKET_TYPE } from '../../utils/constants';
+import { ArrayType } from '../datatypes/arrayType';
 import { JSONType } from '../datatypes/jsonType';
 import { StringType } from '../datatypes/stringType';
 
 const urlInputName = 'URL';
+const bodyInputName = 'Body';
+const headersInputName = 'Headers';
 const outputContentName = 'Content';
 
 export class Get extends PureNode {
@@ -21,6 +23,13 @@ export class Get extends PureNode {
         new StringType(),
         'https://jsonplaceholder.typicode.com/posts'
       ),
+      new Socket(
+        SOCKET_TYPE.IN,
+        headersInputName,
+        new JSONType(),
+        { 'Content-Type': 'application/json' }
+      ),
+
       new Socket(SOCKET_TYPE.OUT, outputContentName, new JSONType(), ''),
     ];
   }
@@ -28,7 +37,46 @@ export class Get extends PureNode {
     inputObject: any,
     outputObject: Record<string, unknown>
   ): Promise<void> {
-    const res = await fetch(inputObject[urlInputName]);
+    const res = await fetch(inputObject[urlInputName], {
+      method: 'Get',
+      headers: inputObject[headersInputName],
+    });
     outputObject[outputContentName] = await res.json();
+  }
+}
+
+export class Post extends PureNode {
+  // default to only manual
+  protected getUpdateBehaviour(): UpdateBehaviour {
+    return new UpdateBehaviour(false, false, 10000);
+  }
+  protected getDefaultIO(): Socket[] {
+    return [
+      new Socket(
+        SOCKET_TYPE.IN,
+        urlInputName,
+        new StringType(),
+        'https://jsonplaceholder.typicode.com/posts'
+      ),
+      new Socket(
+        SOCKET_TYPE.IN,
+        headersInputName,
+        new StringType(),
+        "{ 'Content-Type': 'application/json' }"
+      ),
+      new Socket(SOCKET_TYPE.IN, bodyInputName, new JSONType(), {}),
+      new Socket(SOCKET_TYPE.OUT, outputContentName, new JSONType(), ''),
+    ];
+  }
+  protected async onExecute(
+    inputObject: any,
+    outputObject: Record<string, unknown>
+  ): Promise<void> {
+    const response = await fetch(inputObject[urlInputName], {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: JSON.parse(inputObject[headersInputName]),
+      body: inputObject[bodyInputName], // body data type must match "Content-Type" header
+    });
+    outputObject[outputContentName] = await response.json();
   }
 }
