db019ae5fd698e8895a82cd4105baa9a58763206
Tobias Elinder <tobiaselinder@gmail.com>
2022-03-02 23:09:00 +0100

    allow change address on gateway

--- src/nodes/api/pixotopeGateway.tsx
+++ src/nodes/api/pixotopeGateway.tsx
@@ -14,8 +14,9 @@ const valueName = 'Value';
 const outputContentName = 'Content';
 const nodeName = 'Node';
 const scaleName = 'Scale';
+const addressName = 'Address';
 
-const gatewayAddress = 'http://localhost:16208/gateway/2.0.0/publish';
+const gatewayAddressDefault = 'http://localhost:16208/gateway/2.0.0/publish';
 
 export class PixotopeGatewayGet extends PPNode {
   // default to poll on interval X seconds
@@ -24,6 +25,13 @@ export class PixotopeGatewayGet extends PPNode {
   }
   protected getDefaultIO(): Socket[] {
     return [
+      new Socket(
+        SOCKET_TYPE.IN,
+        addressName,
+        new StringType(),
+        gatewayAddressDefault,
+        false
+      ),
       new Socket(SOCKET_TYPE.IN, targetName, new StringType(), 'Store'),
       new Socket(SOCKET_TYPE.IN, nameName, new StringType(), 'State'),
       new Socket(SOCKET_TYPE.OUT, outputContentName, new JSONType(), ''),
@@ -35,7 +43,8 @@ export class PixotopeGatewayGet extends PPNode {
   ): Promise<void> {
     const target = inputObject[targetName];
     const name = inputObject[nameName];
-    const res = await fetch(gatewayAddress, {
+    const address = inputObject[addressName];
+    const res = await fetch(address, {
       method: 'POST',
       body: JSON.stringify({
         Topic: {
@@ -59,6 +68,13 @@ export class PixotopeGatewaySet extends PPNode {
 
   protected getDefaultIO(): Socket[] {
     return [
+      new Socket(
+        SOCKET_TYPE.IN,
+        addressName,
+        new StringType(),
+        gatewayAddressDefault,
+        false
+      ),
       new Socket(SOCKET_TYPE.IN, targetName, new StringType(), 'Store'),
       new Socket(
         SOCKET_TYPE.IN,
@@ -76,8 +92,8 @@ export class PixotopeGatewaySet extends PPNode {
     const target = inputObject[targetName];
     const name = inputObject[nameName];
     const value = inputObject[valueName];
-
-    fetch(gatewayAddress, {
+    const address = inputObject[addressName];
+    fetch(address, {
       method: 'POST',
       body: JSON.stringify({
         Topic: { Type: 'Set', Target: target, Name: name },
@@ -90,6 +106,13 @@ export class PixotopeGatewaySet extends PPNode {
 export class PixotopeGatewayCall extends PPNode {
   protected getDefaultIO(): Socket[] {
     return [
+      new Socket(
+        SOCKET_TYPE.IN,
+        addressName,
+        new StringType(),
+        gatewayAddressDefault,
+        false
+      ),
       new Socket(SOCKET_TYPE.IN, targetName, new StringType(), 'Store'),
       new Socket(SOCKET_TYPE.IN, methodName, new StringType(), 'VideoIO'),
       new Socket(SOCKET_TYPE.IN, paramsName, new JSONType(), {
@@ -106,8 +129,8 @@ export class PixotopeGatewayCall extends PPNode {
     const target = inputObject[targetName];
     const method = inputObject[methodName];
     const params = inputObject[paramsName];
-
-    const res = await fetch(gatewayAddress, {
+    const address = inputObject[addressName];
+    const res = await fetch(address, {
       method: 'POST',
       body: JSON.stringify({
         Topic: { Type: 'Call', Target: target, Method: method },
@@ -121,6 +144,13 @@ export class PixotopeGatewayCall extends PPNode {
 export class PixotopeGatewayCallSaveImage extends PPNode {
   protected getDefaultIO(): Socket[] {
     return [
+      new Socket(
+        SOCKET_TYPE.IN,
+        addressName,
+        new StringType(),
+        gatewayAddressDefault,
+        false
+      ),
       new Socket(SOCKET_TYPE.IN, targetName, new StringType(), 'Pipeline'),
       new Socket(SOCKET_TYPE.IN, nodeName, new StringType(), 'XXX'),
       new Socket(
@@ -136,7 +166,8 @@ export class PixotopeGatewayCallSaveImage extends PPNode {
     inputObject: any,
     outputObject: Record<string, unknown>
   ): Promise<void> {
-    const res = await fetch(gatewayAddress, {
+    const address = inputObject[addressName];
+    const res = await fetch(address, {
       method: 'POST',
       body: JSON.stringify({
         Topic: {
