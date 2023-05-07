bd58088538eb66d0f9be82954f8cee36039fe6b3
Tobias Elinder <tobiaselinder@gmail.com>
2023-05-07 13:18:44 +0200

    some things done

--- src/nodes/text.tsx
+++ src/nodes/text.tsx
@@ -16,25 +16,26 @@ import { NumberType } from './datatypes/numberType';
 import { ColorType } from './datatypes/colorType';
 
 export class Label extends PPNode {
-  _refText: PIXI.Text;
-  _refTextStyle: PIXI.TextStyle;
-  currentInput: HTMLDivElement;
+  PIXIText: PIXI.Text;
+  PIXITextStyle: PIXI.TextStyle;
+  HTMLTextComponent: HTMLDivElement;
   initialData: any;
 
   constructor(name: string, customArgs?: CustomArgs) {
-    const nodeWidth = 128;
-
     super(name, {
       ...customArgs,
-      nodeWidth,
     });
 
     this.initialData = customArgs?.initialData;
 
-    this._refTextStyle = new PIXI.TextStyle();
-    const basicText = new PIXI.Text('', this._refTextStyle);
-    this._refText = this.addChild(basicText);
-    this._refText.visible = false;
+    this.PIXITextStyle = new PIXI.TextStyle();
+    const basicText = new PIXI.Text('', this.PIXITextStyle);
+    this.PIXIText = this.addChild(basicText);
+    this.PIXIVisible();
+  }
+
+  public getDefaultNodeWidth(): number {
+    return 128;
   }
 
   public getIsPresentationalNode(): boolean {
@@ -62,7 +63,6 @@ export class Label extends PPNode {
   }
 
   protected getDefaultIO(): PPSocket[] {
-    const nodeWidth = 128;
     const fontSize = 32;
     const fillColor = NODE_TYPE_COLOR.OUTPUT;
 
@@ -83,13 +83,6 @@ export class Label extends PPNode {
         TRgba.fromString(fillColor),
         false
       ),
-      new PPSocket(
-        SOCKET_TYPE.IN,
-        'min-width',
-        new NumberType(true, 1),
-        nodeWidth,
-        false
-      ),
     ].concat(super.getDefaultIO());
   }
 
@@ -104,17 +97,59 @@ export class Label extends PPNode {
     if (this.initialData) {
       this.setInputData('Input', this.initialData);
     }
-
-    // if the Node is newly added, focus it so one can start writing
     if (source === NODE_SOURCE.NEW) {
-      this.currentInput = null;
-      this.createInputElement();
-    } else {
-      this._refText.visible = true;
+      this.HTMLVisible();
     }
 
     super.onNodeAdded(source);
   };
+
+  public HTMLVisible() {
+    this.PIXIText.visible = false;
+    this.createInputElement();
+    //this.HTMLTextComponent.hidden = false;
+    this.HTMLTextComponent.focus();
+  }
+  public PIXIVisible() {
+    this.PIXIText.visible = true;
+    if (this.HTMLTextComponent) {
+      document.body.removeChild(this.HTMLTextComponent);
+      //this.HTMLTextComponent.hidden = true;
+    }
+    this.executeOptimizedChain();
+  }
+
+  onPointerClick(event: PIXI.FederatedPointerEvent): void {
+    this.HTMLVisible();
+  }
+
+  protected async onExecute(input, output): Promise<void> {
+    const text = String(input['Input']);
+    const fontSize = Math.max(1, input['fontSize']);
+    //const minWidth = Math.max(1, input['min-width']);
+    const color: TRgba = input['backgroundColor'];
+
+    const marginTopBottom = fontSize / 2;
+    const marginLeftRight = fontSize / 1.5;
+
+    this.PIXITextStyle.fontSize = fontSize;
+    this.PIXITextStyle.lineHeight = fontSize * NOTE_LINEHEIGHT_FACTOR;
+    this.PIXITextStyle.fill = color.isDark()
+      ? TRgba.white().hex()
+      : TRgba.black().hex();
+
+    const textMetrics = PIXI.TextMetrics.measureText(text, this.PIXITextStyle);
+
+    this.resizeAndDraw(
+      Math.max(this.getMinNodeWidth(), textMetrics.width + marginLeftRight * 2),
+      textMetrics.height + marginTopBottom * 2
+    );
+    output['Output'] = text;
+
+    this.PIXIText.text = text;
+    this.PIXIText.x = NODE_MARGIN + marginLeftRight;
+    this.PIXIText.y = marginTopBottom;
+  }
 
   public createInputElement = () => {
     // create html input element
@@ -122,18 +157,18 @@ export class Label extends PPNode {
     const text = this.getInputData('Input');
     const fontSize = this.getInputData('fontSize');
     const color = this.getInputData('backgroundColor');
-    const marginLeftRight = fontSize / 1.5;
     const marginTopBottom = fontSize / 2;
+    const marginLeftRight = fontSize / 1.2;
 
-    this.currentInput = document.createElement('div');
-    this.currentInput.id = 'Input';
-    this.currentInput.contentEditable = 'true';
-    this.currentInput.innerText = text;
+    this.HTMLTextComponent = document.createElement('div');
+    this.HTMLTextComponent.id = 'Input';
+    this.HTMLTextComponent.contentEditable = 'true';
+    this.HTMLTextComponent.innerText = text;
 
     const style = {
       fontFamily: 'Arial',
       fontSize: `${fontSize}px`,
-      lineHeight: `${fontSize * (NOTE_LINEHEIGHT_FACTOR + 0.022)}px`, // 0.022 corrects difference between div and PIXI.Text
+      //lineHeight: `${fontSize * (NOTE_LINEHEIGHT_FACTOR + 0.022)}px`, // 0.022 corrects difference between div and PIXI.Text
       textAlign: 'left',
       margin: NOTE_MARGIN_STRING,
       color: color.isDark() ? TRgba.white().hex() : TRgba.black().hex(),
@@ -149,57 +184,30 @@ export class Label extends PPNode {
       width: `${this.nodeWidth}px`,
       height: `${this.nodeHeight}px`,
     };
-    Object.assign(this.currentInput.style, style);
-
-    // TODO get rid of this, very bad
-    setTimeout(() => {
-      // set caret to end
-      const range = document.createRange();
-      const sel = window.getSelection();
-      range.selectNodeContents(this.currentInput);
-      range.collapse(false);
-      sel.removeAllRanges();
-      sel.addRange(range);
-
-      // set focus
-      this.currentInput.focus();
-    }, 100);
+    Object.assign(this.HTMLTextComponent.style, style);
 
     // add event handlers
-    this.currentInput.addEventListener('blur', (e) => {
-      console.log('blur', e);
-      this.currentInput.remove();
-      this._refText.visible = true;
-      this.doubleClicked = false;
-      this.executeOptimizedChain();
+    this.HTMLTextComponent.addEventListener('blur', (e) => {
+      this.PIXIVisible();
     });
 
-    this.currentInput.addEventListener('input', (e) => {
-      let text = (e as any).target.innerText;
-      this._refText.text = text;
-      const minWidth = this.getInputData('min-width');
+    this.HTMLTextComponent.addEventListener('input', (e) => {
+      const text = (e as any).target.innerText;
+      this.PIXIText.text = text;
+      const minWidth = this.width;
       const textMetrics = PIXI.TextMetrics.measureText(
         text,
-        this._refTextStyle
+        this.PIXITextStyle
       );
 
-      // correct for issue in chrome where pressing enter would add 2 line breaks
-      // so I check for 2 empty line breaks at the end and delete one
-      let textMetricsHeight = textMetrics.height;
-      const length = textMetrics.lines.length;
-      if (
-        textMetrics.lines[length - 1] === '' &&
-        textMetrics.lines[length - 2] === ''
-      ) {
-        text = textMetrics.text.substr(0, textMetrics.text.length - 2);
-        this._refText.text = text;
-        textMetricsHeight = textMetrics.lineHeight * (length - 1);
-      }
+      const textMetricsHeight = textMetrics.height;
 
       const newWidth = textMetrics.width + marginLeftRight * 2;
       const newHeight = textMetricsHeight * NOTE_LINEHEIGHT_FACTOR;
-      this.currentInput.style.width = `${newWidth}px`;
-      this.currentInput.style.height = `${newHeight + marginTopBottom * 2}px`;
+      this.HTMLTextComponent.style.width = `${newWidth}px`;
+      this.HTMLTextComponent.style.height = `${
+        newHeight + marginTopBottom * 2
+      }px`;
 
       this.resizeAndDraw(
         Math.max(minWidth, newWidth),
@@ -210,62 +218,24 @@ export class Label extends PPNode {
       this.executeOptimizedChain();
     });
 
-    document.body.appendChild(this.currentInput);
-  };
-
-  public onNodeDoubleClick = () => {
-    this._refText.visible = false;
-    this.createInputElement();
-  };
-
-  public onExecute = async (input, output) => {
-    const text = String(input['Input']);
-    const fontSize = Math.max(1, input['fontSize']);
-    const minWidth = Math.max(1, input['min-width']);
-    const color: TRgba = input['backgroundColor'];
-
-    const marginTopBottom = fontSize / 2;
-    const marginLeftRight = fontSize / 1.5;
-
-    this._refTextStyle.fontSize = fontSize;
-    this._refTextStyle.lineHeight = fontSize * NOTE_LINEHEIGHT_FACTOR;
-    this._refTextStyle.fill = color.isDark()
-      ? TRgba.white().hex()
-      : TRgba.black().hex();
-
-    const textMetrics = PIXI.TextMetrics.measureText(text, this._refTextStyle);
-
-    this.resizeAndDraw(
-      Math.max(minWidth, textMetrics.width + marginLeftRight * 2),
-      textMetrics.height + marginTopBottom * 2
-    );
-    output['Output'] = text;
-
-    this._refText.text = text;
-    this._refText.x = NODE_MARGIN + marginLeftRight;
-    this._refText.y = marginTopBottom;
-
-    // TODO remove this insane hack
-    setTimeout(() => {
-      this._refText.alpha = this.alpha;
-    }, 110);
+    document.body.appendChild(this.HTMLTextComponent);
   };
 
   // scale input if node is scaled
   public onNodeDragOrViewportMove = () => {
-    if (this.currentInput != null) {
+    if (this.HTMLTextComponent != null) {
       const screenPoint = PPGraph.currentGraph.viewport.toScreen(
         this.x,
         this.y
       );
-      this.currentInput.style.transform = `scale(${PPGraph.currentGraph.viewportScaleX}`;
-      this.currentInput.style.left = `${screenPoint.x}px`;
-      this.currentInput.style.top = `${screenPoint.y}px`;
+      this.HTMLTextComponent.style.transform = `scale(${PPGraph.currentGraph.viewportScaleX}`;
+      this.HTMLTextComponent.style.left = `${screenPoint.x}px`;
+      this.HTMLTextComponent.style.top = `${screenPoint.y}px`;
     }
   };
 
   public onNodeRemoved = () => {
-    this._refText.destroy();
+    this.PIXIText.destroy();
   };
 
   public getShrinkOnSocketRemove(): boolean {
