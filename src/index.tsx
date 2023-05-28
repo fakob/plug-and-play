3acd9d16ac349da9d1161010332361dc1986ff71
Jakob Schindegger <jakob@fakob.com>
2023-05-28 22:37:32 +0200

    Missing quotes

--- src/index.tsx
+++ src/index.tsx
@@ -19,21 +19,21 @@ const description =
 const author = 'a plug and player';
 
 const metaTagHTML = `<!-- Google / Search Engine Tags -->
-<meta itemprop="name" content=${title}>
-<meta itemprop="description" content=${description}>
-<meta itemprop="image" content=${imageURL}>
+<meta itemprop="name" content="${title}">
+<meta itemprop="description" content="${description}">
+<meta itemprop="image" content="${imageURL}">
 <!-- Facebook Meta Tags -->
-<meta property="og:url" content=${url}>
+<meta property="og:url" content="${url}">
 <meta property="og:type" content="website">
-<meta property="og:title" content=${title}>
-<meta property="og:description" content=${description}>
-<meta property="og:image" content=${imageURL}>
+<meta property="og:title" content="${title}">
+<meta property="og:description" content="${description}">
+<meta property="og:image" content="${imageURL}">
 <!-- Twitter Meta Tags -->
 <meta name="twitter:card" content="summary">
-<meta name="twitter:title" content=${title}>
-<meta name="twitter:description" content=${description}>
-<meta name="twitter:image" content=${imageURL}>
-<meta name="twitter:creator" content=${author}>
+<meta name="twitter:title" content="${title}">
+<meta name="twitter:description" content="${description}">
+<meta name="twitter:image" content="${imageURL}">
+<meta name="twitter:creator" content="${author}">
 `;
 
 head.insertAdjacentHTML('afterbegin', metaTagHTML);
