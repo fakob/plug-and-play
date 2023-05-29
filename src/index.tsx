99229372b7365dce2351cbfd9740560a624d974d
Jakob Schindegger <jakob@fakob.com>
2023-05-29 09:57:14 +0200

    change the description length and meta position

--- src/index.tsx
+++ src/index.tsx
@@ -8,21 +8,24 @@ import App from './App';
 import styles from './utils/style.module.css';
 import './utils/global.css';
 
-const head = document.querySelector('head');
+const titleTag = document.querySelector('title');
 
 const title = 'Your Plug and Playground';
 const imageURL =
   'https://plugandplayground.dev/assets/PlugAndPlayground-Drawing-a-chart.png';
 const url = 'https://plugandplayground.dev';
 const description =
-  'A visual toolkit for creative prototyping to explore, transform or visualise data.';
+  'Creative prototyping to explore, transform or visualise data.';
 const author = 'a plug and player';
 
-const metaTagHTML = `<!-- Google / Search Engine Tags -->
+const metaTagHTML = `<!-- Primary Meta Tags -->
+<meta name="title" content="${title}">
+<meta name="description" content="${description}">
+<!-- Google / Search Engine Tags -->
 <meta itemprop="name" content="${title}">
 <meta itemprop="description" content="${description}">
 <meta itemprop="image" content="${imageURL}">
-<!-- Facebook Meta Tags -->
+<!-- Open Graph Meta Tags -->
 <meta property="og:url" content="${url}">
 <meta property="og:type" content="website">
 <meta property="og:title" content="${title}">
@@ -36,7 +39,7 @@ const metaTagHTML = `<!-- Google / Search Engine Tags -->
 <meta name="twitter:creator" content="${author}">
 `;
 
-head.insertAdjacentHTML('afterbegin', metaTagHTML);
+titleTag.insertAdjacentHTML('afterend', metaTagHTML);
 
 const reactElement = document.createElement('div');
 const container = document.body.appendChild(reactElement);
