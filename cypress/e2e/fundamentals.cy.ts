53cd1f49d36b19889874146697613c4eab5a778a
Tobias Elinder <tobiaselinder@gmail.com>
2023-07-31 21:39:35 +0200

    added fundamentals test

--- cypress/e2e/fundamentals.cy.ts
+++ cypress/e2e/fundamentals.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
+describe('fundamentals', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.visit('http://localhost:8080');
+  });
+});
