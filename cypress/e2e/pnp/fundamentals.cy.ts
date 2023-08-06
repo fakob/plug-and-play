6a128116e04cfc062448ff3427aa2acab3c72466
Tobias Elinder <tobiaselinder@gmail.com>
2023-08-06 22:09:49 +0200

    even longer wait

--- cypress/e2e/pnp/fundamentals.cy.ts
+++ cypress/e2e/pnp/fundamentals.cy.ts
@@ -4,7 +4,7 @@ describe('fundamentals', () => {
     cy.visit('http://127.0.0.1:8080');
     cy.wait(2000); // ugly, wait for graphs to arrive
     cy.get('#\\:r3\\:').click();
-    cy.wait(2000); // ugly, wait for graphs to arrive
+    cy.wait(10000); // ugly, wait for graphs to arrive
     cy.get('body').contains("Drawing a chart").click();
   });
 
