0bf5f31b7484a06ba53493524302bdf19d5b0d79
Tobias Elinder <tobiaselinder@gmail.com>
2023-08-06 21:42:46 +0200

    name as well

--- cypress/e2e/pnp/fundamentals.cy.ts
+++ cypress/e2e/pnp/fundamentals.cy.ts
@@ -1,15 +1,10 @@
 /* eslint-disable prettier/prettier */
 describe('fundamentals', () => {
-  it('opens page', () => {
+  it('Open "Drawing a chart" - graph', () => {
     cy.visit('http://127.0.0.1:8080');
     cy.wait(2000); // ugly, wait for graphs to arrive
     cy.get('#\\:r3\\:').click();
     cy.get('body').contains("Drawing a chart").click();
   });
-  /*it('right click', () => {
-    cy.get('.DwtG9OodcaM_pw_oSWFh > canvas').rightclick(); // just background
-  });
-  it('clear', () => {
-    cy.get(':nth-child(8) > .MuiListItemIcon-root').click();
-  });*/
+
 });
