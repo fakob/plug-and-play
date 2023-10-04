c79703c6ba8f8f399d0cea5ee25f9b7529655bdb
Jakob Schindegger <jakob@fakob.com>
2023-10-04 23:09:05 +0200

    Remove commented out code

--- src/HelpContent.tsx
+++ src/HelpContent.tsx
@@ -99,7 +99,6 @@ const HelpContent = (props) => {
   };
 
   useEffect(() => {
-    //updateGraphSearchItems();
     PPStorage.getInstance()
       .getRemoteGraphsList()
       .then((arrayOfFileNames) => {
