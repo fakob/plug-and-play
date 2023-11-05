df60db5746f08280f2eb268c3b0ba05b6e6bb39d
Tobias Elinder <tobiaselinder@gmail.com>
2023-11-05 11:37:18 +0100

    draw bounds

--- src/nodes/draw/drawMeta.tsx
+++ src/nodes/draw/drawMeta.tsx
@@ -5,11 +5,13 @@ import { TRgba } from '../../utils/interfaces';
 import { DeferredPixiType } from '../datatypes/deferredPixiType';
 import { NumberType } from '../datatypes/numberType';
 import { outputPixiName } from './abstract';
+import * as PIXI from 'pixi.js';
 
 const outputXName = 'X';
 const outputYName = 'Y';
 const outputWidthName = 'Width';
 const outputHeightName = 'Height';
+const inputMarginName = 'Margin';
 
 export default class DRAw_Get_Bounds extends PPNode {
   public getName(): string {
@@ -30,6 +32,12 @@ export default class DRAw_Get_Bounds extends PPNode {
   public getDefaultIO(): Socket[] {
     return [
       new Socket(SOCKET_TYPE.IN, outputPixiName, new DeferredPixiType()),
+      new Socket(
+        SOCKET_TYPE.IN,
+        inputMarginName,
+        new NumberType(false, 0, 100),
+        0,
+      ),
       new Socket(SOCKET_TYPE.OUT, outputXName, new NumberType()),
       new Socket(SOCKET_TYPE.OUT, outputYName, new NumberType()),
       new Socket(SOCKET_TYPE.OUT, outputWidthName, new NumberType()),
@@ -37,5 +45,27 @@ export default class DRAw_Get_Bounds extends PPNode {
     ];
   }
 
-  protected onExecute(input: any, output: any): Promise<void> {}
+  public static getDrawingBounds(drawingFunction, margin: number) {
+    const tempContainer = new PIXI.Container();
+    drawingFunction(tempContainer, {});
+    const bounds = tempContainer.getBounds();
+    bounds.x -= margin;
+    bounds.y -= margin;
+    bounds.width += margin * 2;
+    bounds.height += margin * 2;
+    return bounds;
+  }
+
+  protected onExecute(input: any, output: any): Promise<void> {
+    const drawingFunction = input[outputPixiName];
+    const bounds = DRAw_Get_Bounds.getDrawingBounds(
+      drawingFunction,
+      input[inputMarginName],
+    );
+    output[outputXName] = bounds.x;
+    output[outputYName] = bounds.y;
+    output[outputWidthName] = bounds.width;
+    output[outputHeightName] = bounds.height;
+    return;
+  }
 }
