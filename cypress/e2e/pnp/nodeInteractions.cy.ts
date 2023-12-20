83a596bf1a9ad0e024daa51d3c293e5462506cd0
Tobias Elinder <tobiaselinder@gmail.com>
2023-12-20 21:46:47 +0100

    maybe finished POC, added basic click socket test

--- cypress/e2e/pnp/nodeInteractions.cy.ts
+++ cypress/e2e/pnp/nodeInteractions.cy.ts
@@ -7,5 +7,20 @@ describe('nodeInteractions', () => {
     cy.window().then(win => {
       expect((win as any).testController.identify()).to.eq("its testcontroller");
     });
+    cy.wait(100);
+    cy.window().then(win => {
+      expect((win as any).testController.addNode("Add")).to.eq(true);
+    });
   });
+
+  it ("Click socket and open menu", () => {
+    cy.window().then(win => {
+      const coordinates = (win as any).testController.getSocketCenterByNodeTypeAndSocketName("Add", "Added");
+      console.log("coordinates: " + coordinates[0])
+      cy.get('body').click(coordinates[0], coordinates[1]);
+      cy.get("body").should("contain", "Shift+Click to add to dashboard");
+
+      });
+    });
+
 });
--- src/TestController.ts
+++ src/TestController.ts
@@ -1,5 +1,33 @@
+import PPGraph from './classes/GraphClass';
+import PPNode from './classes/NodeClass';
+
 export default class TestController {
   identify(): string {
     return 'its testcontroller';
+  }
+
+  addNode(nodeName): boolean {
+    PPGraph.currentGraph.addNewNode(nodeName);
+    return true;
+  }
+
+  getNodeByType(nodeType: string): PPNode {
+    const nodes = Object.values(PPGraph.currentGraph.nodes);
+    return nodes.find((node) => node.type == nodeType);
+  }
+  getNodeCenterByType(nodeType: string): [number, number] {
+    const toReturn = this.getNodeByType(nodeType);
+    return [toReturn.x + toReturn.width / 2, toReturn.y + toReturn.height / 2];
+  }
+
+  getSocketCenterByNodeTypeAndSocketName(nodeType: string, socketName: string) {
+    const node = this.getNodeByType(nodeType);
+    const socket = node
+      .getAllSockets()
+      .find((socket) => socket.name == socketName);
+    return [
+      node.x + socket.x + socket._SocketRef.x + socket._SocketRef.width / 2,
+      node.y + socket.y + socket._SocketRef.y + socket._SocketRef.height / 2,
+    ];
   }
 }
