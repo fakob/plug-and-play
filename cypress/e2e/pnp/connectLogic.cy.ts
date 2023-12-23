a1d1f4cce9d90e87d8f81398875d0207ff94b98f
Tobias Elinder <tbe@futureuniverse.com>
2023-12-23 19:27:04 +0100

    better test

--- cypress/e2e/pnp/connectLogic.cy.ts
+++ cypress/e2e/pnp/connectLogic.cy.ts
@@ -1,12 +1,7 @@
 
 describe('connectLogic', () => {
-  it('Get Test Controller', () => {
-
+  it('Add nodes', () => {
     cy.visit('http://127.0.0.1:8080/?new=true');
-
-    cy.window().then(win => {
-      expect((win as any).testController.identify()).to.eq("its testcontroller");
-    });
     cy.wait(100);
     cy.window().then(win => {
       const anyWin = win as any;
@@ -14,14 +9,27 @@ describe('connectLogic', () => {
       expect(anyWin.testController.addNode("Subtract", "Subtract")).to.eq(true);
       expect(anyWin.testController.addNode("Constant", "Constant")).to.eq(true);
     });
-
     cy.wait(100);
     cy.window().then(win => {
       const anyWin = win as any;
+      expect(anyWin.testController.getNodeByID("Add").id).to.eq("Add");
+    });
+  });
+
+  it("move nodes", () => {
+    cy.wait(100);
+    cy.window().then(win => {
+      const anyWin = win as any;
+      const constantNode = anyWin.testController.getNodeByID("Constant");
+      const xPre = constantNode.x;
       anyWin.testController.moveNodeByID("Constant", -200, 0);
+      const xPost = constantNode.x;
+      expect(xPost - xPre).to.eq(-200);
       anyWin.testController.moveNodeByID("Subtract", 0, 200);
     });
+  });
 
+  it("connect nodes", () => {
     cy.wait(100);
     cy.window().then(win => {
       const anyWin = win as any;
@@ -36,8 +44,7 @@ describe('connectLogic', () => {
       const anyWin = win as any;
       expect(anyWin.testController.getNodeOutputValue("Add", "Added")).to.eq(10);
     });
-
   });
 
-
 });
+
