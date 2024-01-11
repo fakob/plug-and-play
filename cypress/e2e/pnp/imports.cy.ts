f515a073e795d1d117b1be9beddd0b0c8117011c
Tobias Elinder <tobiaselinder@gmail.com>
2024-01-11 19:56:28 +0100

    add test that would have caught previous bug

---
+++ cypress/e2e/pnp/imports.cy.ts
@@ -0,0 +1,33 @@
+import { doWithTestController } from "./helpers";
+describe('importing third party libraries and using', () => {
+
+  it('Add nodes', () => {
+    cy.visit('http://127.0.0.1:8080/?new=true');
+    cy.wait(100);
+    doWithTestController(testController => {
+      expect(testController.addNode("LoadNPM", "LoadNPM")).to.eq(true);
+      expect(testController.addNode("CustomFunction", "CustomFunction")).to.eq(true);
+    });
+  });
+  it ("Set up nodes, connect them", () => {
+    cy.wait(100);
+    doWithTestController(testController => {
+      // set it to load chartjs
+      testController.setNodeInputValue("LoadNPM", "packageName", "chart.js");
+      testController.moveNodeByID("CustomFunction", 200,0);
+      testController.connectNodesByID("LoadNPM", "CustomFunction", "NpmPackage");
+      testController.executeNodeByID("LoadNPM");
+    });
+  })
+  it ("See that the imported module makes it through as expected (once all is loaded)", () => {
+    cy.wait(4000);
+    // see that its there and loaded
+    doWithTestController(testController => {
+        testController.executeNodeByID("CustomFunction");
+    });
+    cy.wait(300);
+    doWithTestController(testController => {
+        expect(testController.getNodeOutputValue("CustomFunction","OutData").defaults).to.not.be.undefined; // we check that this function has survived through all this
+    });
+  });
+});
