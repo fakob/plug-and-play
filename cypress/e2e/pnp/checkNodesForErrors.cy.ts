8a1440e2b9845dcece279bb46e3f3d98d233d01d
Tobias Elinder <tobiaselinder@gmail.com>
2024-01-13 09:54:55 +0100

    add new inactive test that breaks because of latent bugs

--- cypress/e2e/pnp/checkNodesForErrors.cy.ts
+++ cypress/e2e/pnp/checkNodesForErrors.cy.ts
@@ -2,21 +2,25 @@ import { doWithTestController } from "./helpers";
 
 // TODO add all nodes
 describe('checkNodesForErrors', () => {
+    let numGoodNodes = 0;
     it("add one of each node", () => {
       cy.visit('http://127.0.0.1:8080/?new=true');
       cy.wait(100);
       let allNodeTypes = [];
+      const nodesWithKnownErrors: string[] = ["SqliteReader", "Image", "JSONSet", "PixotopeGatewayCall","DRAW_Shape","Table_GetColumnByName","PixotopeGatewayGet"]; // TODO FIX THESE !!!!
+      let goodNodes = [];
 
       // Fetch all node types and process them within the same test
       doWithTestController(testController => {
-        allNodeTypes = testController.getAllDefinedNodeTypes();
-        cy.log("all node types: " + JSON.stringify(allNodeTypes));
+          allNodeTypes = testController.getAllDefinedNodeTypes();
+          cy.log("all node types: " + JSON.stringify(allNodeTypes));
+          numGoodNodes = goodNodes.length;
         });
         cy.wait(100);
 
         doWithTestController(testController => {
-            const nodesWithKnownErrors: string[] = ["SqliteReader", "Image", "JSONSet", "PixotopeGatewayCall","DRAW_Shape","Table_GetColumnByName","PixotopeGatewayGet"]; // TODO FIX THESE !!!!
-            allNodeTypes.filter(nodeType => (nodesWithKnownErrors.find(t2 => nodeType == t2) == undefined)).forEach((nodeType, index) => {
+            goodNodes = allNodeTypes.filter(nodeType => (nodesWithKnownErrors.find(t2 => nodeType == t2) == undefined));
+            goodNodes.forEach((nodeType, index) => {
                 if (index < 100) {
                     cy.log("adding node: " + nodeType);
                     testController.addNode(nodeType, nodeType);
@@ -33,5 +37,18 @@ describe('checkNodesForErrors', () => {
             });
         });
     });
+    it("save graph", () => {
+        cy.get("body").type("{ctrl}s");
+    });
+    // TODO RE-ENABLE AFTER BUGFIXES, THIS SHOULDNT BREAK!
+    /*it("see that they are all there after loading again", () => {
+      cy.visit('http://127.0.0.1:8080');
+        cy.wait(1000);
+        doWithTestController(testController => {
+            expect(testController.getNodes().length).to.eq(numGoodNodes);
+
+        });
+    });
+    */
 
 });
